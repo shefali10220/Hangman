1fa7c3bb12ce7c0a4d444df016b95dfdbe162073
shefali10220 <75357274+shefali10220@users.noreply.github.com>
2021-05-25 09:49:57 +0530

    Add files via upload

---
+++ w_hint_main.py
@@ -0,0 +1,155 @@
+import random
+from word import word_list
+
+
+def get_word():
+    entry_list = list(word_list.items())
+    random_entry = random.choice(entry_list)
+    words = list()
+    words.append(random_entry)
+    '''print(type(words))
+    print(words)'''
+    actual_word = list()
+    word = words[0][0]
+    hint = words[0][1]
+    return word.upper(), hint.upper()
+
+
+def play(word, hint):
+    word_completion = "_" * len(word)
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    tries = 6
+    print(display_hangman(tries))
+    print("Hint:", hint)
+    print()
+    print(word_completion)
+    print("\n")
+    while not guessed and tries > 0:
+        guess = input("Please guess a letter or word: ").upper()
+        if len(guess) == 1 and guess.isalpha():
+            if guess in guessed_letters:
+                print("You already guessed the letter", guess)
+            elif guess not in word:
+                print(guess, "is not in the word.")
+                tries -= 1
+                guessed_letters.append(guess)
+            else:
+                print("Good job,", guess, "is in the word!")
+                guessed_letters.append(guess)
+                word_as_list = list(word_completion)
+                indices = [i for i, letter in enumerate(word) if letter == guess]
+                for index in indices:
+                    word_as_list[index] = guess
+                word_completion = "".join(word_as_list)
+                if "_" not in word_completion:
+                    guessed = True
+        elif len(guess) == len(word) and guess.isalpha():
+            if guess in guessed_words:
+                print("You already guessed the word", guess)
+            elif guess != word:
+                print(guess, "is not the word.")
+                tries -= 1
+                guessed_words.append(guess)
+            else:
+                guessed = True
+                word_completion = word
+        else:
+            print("Not a valid guess.")
+        print(display_hangman(tries))
+        print(word_completion)
+        print("\n")
+    if guessed:
+        print("Congrats, you guessed the word! You win!")
+    else:
+        print("Sorry, you ran out of tries. The word was " + word + ". Maybe next time!")
+
+
+def display_hangman(tries):
+    stages = [  # final state: head, torso, both arms, and both legs
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / \\
+                   -
+                """,
+                # head, torso, both arms, and one leg
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / 
+                   -
+                """,
+                # head, torso, and both arms
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |      
+                   -
+                """,
+                # head, torso, and one arm
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|
+                   |      |
+                   |     
+                   -
+                """,
+                # head and torso
+                """
+                   --------
+                   |      |
+                   |      O
+                   |      |
+                   |      |
+                   |     
+                   -
+                """,
+                # head
+                """
+                   --------
+                   |      |
+                   |      O
+                   |    
+                   |      
+                   |     
+                   -
+                """,
+                # initial empty state
+                """
+                   --------
+                   |      |
+                   |      
+                   |    
+                   |      
+                   |     
+                   -
+                """
+    ]
+    return stages[tries]
+
+
+def main():
+    name = input("Enter the name of the player")
+    print("Hi,", name, "Lets's play Hangman!")
+    print("You have 6 tries in total,guess wisely")
+    word, hint = get_word()
+    play(word, hint)
+    while input("Wanna Play Again? (Y/N) ").upper() == "Y":
+        word, hint = get_word()
+        play(word, hint)
+
+
+main()
---
+++ word_hint.py
@@ -0,0 +1,25 @@
+import random
+word_list ={
+        'soup':'food',
+        'red': 'color',
+        'blue': 'color',
+        'tired': 'worn out',
+        'backpack': 'a place where one carries belongings',
+        'minute':'immeasurably small',
+        'evident':'clearly revealed to the mind or the senses or judgment ',
+        'concern':'something that interests you because it is important',
+        'approach':'move towards',
+        'establish':'set up or found'
+          }
+
+'''entry_list = list(word_list.items())
+random_entry = random.choice(entry_list)
+
+word = list()
+word.append(random_entry)
+print(type(word))
+print(word)
+actual_word = list()
+l = word[0][1]
+print(l)'''
+
